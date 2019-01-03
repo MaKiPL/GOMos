93dcee91790c3d7a708bf2d9581ba3fbc74d1b8a
MaKiPL <makipol@gmail.com>
2019-01-03 17:49:22 +0100

    F2: Fully functional

--- F2Mode.asm
+++ F2Mode.asm
@@ -19,6 +19,85 @@ CMP AH, 0x51
 JE F2_scrollDown
 CMP AH, 0x49
 JE F2_scrollUp
+CMP AH, 0x47 ;home
+JE F2_home
+CMP AH, 0x4F ;end
+JE F2_end
+
+;if ( (AL>= 30 && AL <= 39) || (AL>=41 && AL<= 46) )
+CMP AL, 0x29
+JBE F2Input_loc1
+CMP AL, 0x39
+JBE F2InputEdit
+
+F2Input_loc1:
+CMP AL, 0x61
+JB F2Input_ret
+CMP AL, 0x66
+JA F2Input_ret
+
+F2InputEdit:
+CALL F2_setCursor ;because 0x0E advanced cursor
+;now calculate the address
+MOV BX, [f2_curAdr]
+MOV AX, [f2_cursorAdr]
+SHL AL, 4 ;*16
+PUSH CX
+XOR CX, CX
+MOVZX CX, AL
+ADD BX, CX ;row*16
+MOVZX CX, AH
+SHR CX, 1 ;divides by two because hex edit is in 4bit mode
+ADD BX, CX ;+column
+POP CX
+MOV AX, [f2_inputSafevar]
+CMP AL, 0x39
+JBE F2InputEdit_digit
+SUB AL, 0x57
+JMP f2Inputloc2
+F2InputEdit_digit:
+SUB AL, 0x30
+f2Inputloc2:
+PUSH CX
+MOV CX, [f2_cursorAdr]
+AND CH, 1 ;MOD CX 2
+TEST CH, CH
+MOV CL, [BX]
+JZ f2Inputloc3
+AND CL, 0xF0
+ADD CL, AL
+JMP f2Inputloc4
+f2Inputloc3:
+AND CL, 0x0F
+SHL AL, 4
+ADD CL, AL
+f2Inputloc4:
+XCHG CL, AL
+POP CX
+MOV [BX], AL
+CALL F2_safeAdvanceCursor
+RET
+
+F2Input_ret:
+RET
+
+F2_safeAdvanceCursor:
+MOV AX, [f2_cursorAdr]
+CMP AH, 0x1F
+JB F2sfcloc1
+RET
+F2sfcloc1:
+INC AH
+MOV [f2_cursorAdr], AX
+RET
+
+
+F2_home:
+MOV [f2_cursorAdr+1], byte 0
+RET
+
+F2_end:
+MOV [f2_cursorAdr+1], byte 0x1F
 RET
 
 F2_left:
@@ -38,6 +117,7 @@ RET
 INC AH
 MOV [f2_cursorAdr], AX
 RET
+
 
 F2_up:
 MOV AX, [f2_cursorAdr]
@@ -144,13 +224,7 @@ MOV BX, WORD [0x601]
 JMP printhex
 
 f2_indexWritten:
-MOV AX, [f2_cursorAdr] ;AH= column AL= row
-ADD AH, 6 ;because of '0000: '
-PUSH AX
-SHR AX, 8
-PUSH AX
-CALL SetCursor
-ADD SP, 4
+CALL F2_setCursor
 
 MOV AH, 0x08
 XOR BH, BH
@@ -162,4 +236,14 @@ INT 0x10
 
 
 
+RET
+
+F2_setCursor:
+MOV AX, [f2_cursorAdr] ;AH= column AL= row
+ADD AH, 6 ;because of '0000: '
+PUSH AX
+SHR AX, 8
+PUSH AX
+CALL SetCursor
+ADD SP, 4
 RET
